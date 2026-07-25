5c8c74ddc2c7b7df55fd5c7358ad608e3c319d39
Brian C <283823263+reisntea@users.noreply.github.com>
2026-07-25 16:56:25 -0400

    Add more functionality and add basic layout
    Made a template for what I want the website to look like. Also added the
    ability to remove tasks and also the ability to get projects and tasks
    by index.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./styles.css";
 import { format, parse } from "date-fns";
 
 class Task {
@@ -34,8 +35,12 @@ class Project {
         this.tasks.push(new Task(title, description, dueDate, priority));
     }
 
+    getTaskAtIndex (index) {
+        return this.tasks[index];
+    }
+
     // Sends an array containing all the tasks in the project formatted
-    getAllTasks () {
+    formatAllTasks () {
         return this.tasks.map((task) => task.formatTask());
     }
 
@@ -54,37 +59,48 @@ function ProjectController () {
         projects.push(new Project(`${name}`));
     };
 
-    // Specific projects are going to be accessed by their names using this function
-    const getProject = (name) => projects.find((project) => project.name === name);
+    const getProjectByIndex = (index) => projects[index];
 
-    // Runs the addTask method to the project with the given name
-    const addTaskToProject = (projectName, title, description, dueDate, priority) => {
-        getProject(projectName).addTask(title, description, dueDate, priority);
-    }
+    const getTask = (projectIndex, tasksIndex) => projects[projectIndex].getTaskAtIndex(tasksIndex);
 
+    const formatAllTasksByIndex = (index) => getProjectByIndex(index).formatAllTasks();
 
-    // These functions are for console functionality
     const getAllProjectNames = () => projects.map((project) => project.name);
 
+    // Runs the addTask method to the project at the given index
+    const addTaskToProject = (index, title, description, dueDate, priority) => {
+        getProjectByIndex(index).addTask(title, description, dueDate, priority);
+    }
+
+    const removeTask = (projectIndex, tasksIndex) => {
+        getProjectByIndex(projectIndex).tasks.splice(tasksIndex, 1);
+    };
+
+    // These functions are for console functionality
     const printAllProjectNames = () => {
         console.log(`${getAllProjectNames().join(", ")}`);
     }
 
-    const getAllTasksByProjectName = (projectName) => getProject(projectName).getAllTasks();
+    const printTask = (projectIndex, tasksIndex) => {
+        console.log(getTask(projectIndex, tasksIndex).formatTask())
+    };
 
-    const printAllTasksByProjectName = (projectName) => {
-        console.log(`In ${projectName} \n${getProject(projectName).getAllTasks().join("\n \n")}`);
+    const printAllTasksByIndex = (index) => {
+        console.log(`In ${projects[index].name} \n${getProjectByIndex(index).formatAllTasks().join("\n \n")}`);
     };
 
     const printAll = () => {
-        projects.forEach((project) => printAllTasksByProjectName(project.name));
+        console.log("Printing all...");
+        projects.forEach((project) => printAllTasksByIndex(projects.indexOf(project)));
     }
 
     return {
         addProject,
         addTaskToProject,
+        removeTask,
         printAllProjectNames,
-        printAllTasksByProjectName,
+        printTask,
+        printAllTasksByIndex,
         printAll,
     };
 }
@@ -93,13 +109,14 @@ function ProjectController () {
 window.allProjects = ProjectController();
 allProjects.addProject("Stuff Test");
 allProjects.addProject("Stuff Test 2");
-allProjects.addTaskToProject("Stuff Test", "task 1", "some thingy to do", "01/02/2020", 1);
-allProjects.addTaskToProject("Stuff Test", "task 2", "some thingy to do", "01/02/2020", 5);
-allProjects.addTaskToProject("Stuff Test 2", "tasky", "thingy to do", "01/23/2020", 4);
-
-
-allProjects.printAllProjectNames();
+allProjects.addTaskToProject(0, "task 1", "some thingy to do", "01/02/2020", 1);
+allProjects.addTaskToProject(0, "task 2", "some thingy to do", "01/02/2020", 5);
+allProjects.addTaskToProject(1, "tasky", "thingy to do", "01/23/2020", 4);
+allProjects.printTask(1, 0);
 
 allProjects.printAll();
+allProjects.removeTask(0, 1);
+allProjects.printAll();
 
-// Need to add ability to remove and edit tasks and projects.
+
+// Need to add ability to edit tasks and projects.
